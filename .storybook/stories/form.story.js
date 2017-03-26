bde5ee2e8ededf475ed5ee14b3a9f26488eab33e
Janci Krajnak <jan.krajnak@email.cz>
2017-03-26 11:03:09 +0200

    1.0.1 Updated packages and storybook

--- .storybook/stories/form.story.js
+++ .storybook/stories/form.story.js
@@ -51,5 +51,62 @@ storiesOf('ValidForm', module)
 `,
 `const formProps = {autoComplete: 'off', onSubmit: action}
 const userProps = {label: 'Username', name: 'username', required: true, errorMessages: 'Fill Username!'}
-const passProps = { label: 'Password', name: 'password', required: true, errorMessages: 'Fill Password!'}
+const passProps = { label: 'Password', name: 'password', required: true, errorMessages: 'Fill Password!', type: 'password'}
+`)))
+  .add('error panel mode', () => (
+    wrapper(
+      <ValidForm errorPanel autoComplete="off" onSubmit={action('Form Payload')}>
+        <Field label='Username' name='username' required={true} errorMessages='Fill username'/>
+        <Field label='Email' name='email' required={true} errorMessages='Fill email'/>
+        <Button>Submit</Button>
+      </ValidForm>,
+`<ValidStyles>
+  <ValidForm {...formProps}>
+    <Field {...userProps} />
+    <Field {...passProps} />
+    <Button>Submit</Button>
+  </ValidForm>
+ </ValidStyles>
+`,
+`const formProps = {errorPanel: true, autoComplete: 'off', onSubmit: action}
+const userProps = {label: 'Username', name: 'username', required: true, errorMessages: 'Fill Username!'}
+const passProps = { label: 'Email', name: 'email', required: true, errorMessages: 'Fill Email!'}
+`)))
+  .add('read-only mode', () => (
+    wrapper(
+      <ValidForm readOnly autoComplete="off" onSubmit={action('Form Payload')}>
+        <Field value='John Wick' label='Username' name='username' required={true} errorMessages='Fill username'/>
+        <Field value='john.wick@movies.com' label='Email' name='email' required={true} errorMessages='Fill email'/>
+        <Button>Submit</Button>
+      </ValidForm>,
+`<ValidStyles>
+  <ValidForm {...formProps}>
+    <Field {...userProps} />
+    <Field {...passProps} />
+    <Button>Submit</Button>
+  </ValidForm>
+ </ValidStyles>
+`,
+`const formProps = {readOnly: true, autoComplete: 'off', onSubmit: action}
+const userProps = {label: 'Username', name: 'username', required: true, errorMessages: 'Fill Username!'}
+const passProps = { label: 'Email', name: 'email', required: true, errorMessages: 'Fill Email!'}
+`)))
+  .add('external errors after submit', () => (
+    wrapper(
+      <ValidForm errors={{username: 'Username already exists!'}} autoComplete="off" onSubmit={action('Form Payload')}>
+        <Field value='John Wick' label='Username' name='username' required={true} errorMessages='Fill username'/>
+        <Field value='john.wick@movies.com' label='Email' name='email' required={true} errorMessages='Fill email'/>
+        <Button>Submit</Button>
+      </ValidForm>,
+`<ValidStyles>
+  <ValidForm {...formProps}>
+    <Field {...userProps} />
+    <Field {...passProps} />
+    <Button>Submit</Button>
+  </ValidForm>
+ </ValidStyles>
+`,
+`const formProps = {errors: {username: 'Username already exists!'}, autoComplete: 'off', onSubmit: action}
+const userProps = {label: 'Username', name: 'username', required: true, errorMessages: 'Fill Username!'}
+const passProps = { label: 'Email', name: 'email', required: true, errorMessages: 'Fill Email!'}
 `)))
